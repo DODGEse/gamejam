2a486f880ec240e29dd5798afcbd3ff42d0c290d
DODGEse <222722961+DODGEse@users.noreply.github.com>
2026-03-16 18:00:46 +0700

    deleted interact

--- game/Assets/FILES/Scripts/locations/TransferSO.cs
+++ game/Assets/FILES/Scripts/locations/TransferSO.cs
@@ -3,5 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TransferSO", menuName = "Scriptable Objects/TransferSO")]
 public class TransferSO : ScriptableObject
 {
-    
+    public enum Village
+    {
+        lvl1,
+        lvl2,
+        lvl3,
+        lvl4
+    }
 }
