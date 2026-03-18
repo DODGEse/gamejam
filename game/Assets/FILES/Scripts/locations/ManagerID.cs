8786baca8e96cace92c6b869efde8051f41a9227
DODGEse <222722961+DODGEse@users.noreply.github.com>
2026-03-18 07:54:27 +0700

    ok

--- game/Assets/FILES/Scripts/locations/ManagerID.cs
+++ game/Assets/FILES/Scripts/locations/ManagerID.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,17 +6,31 @@ public class ManagerID : MonoBehaviour, IInteractable
 
 {
     public string sceneID;
+    public SpriteRenderer KeyE;
 
-    // void OnTriggerStay2D(Collider2D collision)
-    // {
-    //     if(Input.GetKey(KeyCode.E) && CompareTag("Player"))
-    //     {
-    //         SceneManager.LoadScene(sceneID);
-    //     }
-    // }
+    void Start()
+    {
+        KeyE.enabled = false;
+    }
 
     public void Interact()
     {
         SceneManager.LoadScene(sceneID);
+        Debug.Log("Interact");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+        KeyE.enabled = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            KeyE.enabled = false;
+        }
     }
 }
